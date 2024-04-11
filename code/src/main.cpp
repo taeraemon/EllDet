a9796d42ba9ce31ced25dfafa6140f6c3e28bb57
taeraemon <dbsl0512@naver.com>
2024-04-11 14:49:06 +0900

    Modify main (filepath or path check)

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <filesystem>
 
 using namespace std;
 using namespace cv;
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]){
 	if(argc != 2){
@@ -14,31 +16,35 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 
-	auto start = clock();
-	EllipseDetector ellipse_Detector;
-	std::vector<Ellipse> ellipses;
+	std::string path = argv[1];
+	if(fs::exists(path)){
+		if(fs::is_regular_file(path)){
+			auto start = clock();
+			EllipseDetector ellipse_Detector;
+			std::vector<Ellipse> ellipses;
 
-	// Img Read
-	std::string imagePath = argv[1];
-	cv::Mat img = cv::imread(imagePath);
-	if(img.empty()){
-		std::cerr << "Error: Image could not be read." << std::endl;
-        return 1;
+			// Img Read
+			cv::Mat img = cv::imread(path);
+			if(img.empty()){
+				std::cerr << "Error: Image could not be read." << std::endl;
+				return 1;
+			}
+
+			// Ellipse Detection
+			ellipses = ellipse_Detector.DetectImage(img);
+
+			// Make New Image
+			cv::Mat3b img0 = ellipse_Detector.image();
+
+			// Darw Ellipse
+			draw_ellipses_all(ellipses, img0);
+
+			// Result Print
+			printf("Time: %.2f ms\n", (clock() - start) * 1000.0 / CLOCKS_PER_SEC);
+			cv::imshow("Result", img0);
+			cv::waitKey();
+		}
 	}
-
-	// Ellipse Detection
-	ellipses = ellipse_Detector.DetectImage(img);
-
-	// Make New Image
-	cv::Mat3b img0 = ellipse_Detector.image();
-
-	// Darw Ellipse
-	draw_ellipses_all(ellipses, img0);
-
-	// Result Print
-	printf("Time: %.2f ms\n", (clock() - start) * 1000.0 / CLOCKS_PER_SEC);
-	cv::imshow("Result", img0);
-	cv::waitKey();
 
 	return 0;
 }
