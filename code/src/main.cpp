19b4792e8316e902ec58b5c43145444b833da00b
taeraemon <dbsl0512@naver.com>
2024-04-11 14:19:36 +0900

    Update run method (input path)

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -8,18 +8,37 @@
 using namespace std;
 using namespace cv;
 
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		std::cerr << "Usage: " << argv[0] << " <Image File or Directory Path>" << std::endl;
+		return 1;
+	}
 
-int main()
-{
 	auto start = clock();
 	EllipseDetector ellipse_Detector;
 	std::vector<Ellipse> ellipses;
-	cv::Mat img = cv::imread(R"(/home/tykim/Documents/project_cpp/EllDet/code/a.jpg)");
+
+	// Img Read
+	std::string imagePath = argv[1];
+	cv::Mat img = cv::imread(imagePath);
+	if(img.empty()){
+		std::cerr << "Error: Image could not be read." << std::endl;
+        return 1;
+	}
+
+	// Ellipse Detection
 	ellipses = ellipse_Detector.DetectImage(img);
+
+	// Make New Image
 	cv::Mat3b img0 = ellipse_Detector.image();
+
+	// Darw Ellipse
 	draw_ellipses_all(ellipses, img0);
+
+	// Result Print
 	printf("Time: %.2f ms\n", (clock() - start) * 1000.0 / CLOCKS_PER_SEC);
 	cv::imshow("Result", img0);
 	cv::waitKey();
+
 	return 0;
 }
